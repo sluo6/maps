99d31cb3eb079c43e2db18b3fd6b36b8afb0284e
Shangwen Luo <shangwenluo6@gmail.com>
2017-05-13 14:42:35 -0500

    started this lab

---
+++ Map/BSTMap/BSTMap.java
@@ -0,0 +1,62 @@
+package BSTMap;
+
+import java.util.Iterator;
+import java.util.Set;
+
+public class BSTMap implements Map61B{
+
+	@Override
+	public Iterator iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean containsKey(Object key) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Object get(Object key) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void put(Object key, Object value) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Set keySet() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object remove(Object key) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object remove(Object key, Object value) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
