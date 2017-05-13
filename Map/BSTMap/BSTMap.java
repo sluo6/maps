348805f75d7e3cb5ece33cf15822e0485d9d238b
Shangwen Luo <shangwenluo6@gmail.com>
2017-05-13 17:57:28 -0500

    defined Node, added size() function

--- Map/BSTMap/BSTMap.java
+++ Map/BSTMap/BSTMap.java
@@ -4,13 +4,35 @@ import java.util.Iterator;
 import java.util.Set;
 
 public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V>{
+	private Node root;		//root of BSTMap
+	
+//Node definition
+	private class Node {
+		private K key;		//sorted by key
+		private V val;		//associated data
+		private Node left, right;		//number of nodes in subtree
+		private int size;		//number of nodes in subtree
+		
+		public Node (K key, V val, int size) {
+			this.key = key;
+			this.val = val;
+			this.size = size;
+		}
+	}
+/** Initializes an empty BSTMap.	
+ */
+public BSTMap() {
+}
+
 
 	@Override
 	public Iterator<K> iterator() {
 		throw new UnsupportedOperationException("iterator is not supported!");
 	}
 
+	
 	@Override
+	/** Removes all of the mappings from this map. */
 	public void clear() {
 		// TODO Auto-generated method stub
 		
@@ -30,8 +52,13 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V>{
 
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
-		return 0;
+		return size(root);
+	}
+	
+	/** return number of k-v pairs in BSTMap rooted at x. */
+	private int size(Node x) {
+		if (x == null) return 0;
+		else return x.size;
 	}
 
 	@Override
