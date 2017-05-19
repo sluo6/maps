3fd8eefa98b85e58fe7871c38db33eb111831ab9
Shangwen Luo <shangwenluo6@gmail.com>
2017-05-18 21:19:31 -0500

    created MyHashMap

---
+++ Map/BSTMap/MyHashMap.java
@@ -0,0 +1,68 @@
+package BSTMap;
+
+import java.util.Iterator;
+import java.util.Set;
+
+public class MyHashMap implements Map61B {
+	
+	public MyHashMap();
+	public MyHashMap(int initialSize);
+	public MyHashMap(int initialSize, double loadFactor);
+
+	@Override
+	public Iterator iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean containsKey(Object key) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Object get(Object key) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void put(Object key, Object value) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Set keySet() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object remove(Object key) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object remove(Object key, Object value) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+
+}
